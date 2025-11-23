22201f513352741805f26e021c07dc76fe65c376
Shivashankar-Chilshetty <chilshetty77@gmail.com>
2025-11-23 17:54:33 +0530

    Implemented writer & critique nodes

--- src/state.ts
+++ src/state.ts
@@ -3,5 +3,6 @@ import { Annotation, MessagesAnnotation } from "@langchain/langgraph";
 // maintain custom state annotations here
 
 export const State = Annotation.Root({
-  ...MessagesAnnotation.spec    // inherit message history annotation
+  ...MessagesAnnotation.spec,    // inherit message history annotation
+  revisions: Annotation<number>   // custom annotation to track number of revisions(i,e number of times critique sends feedback to writer)
 });
